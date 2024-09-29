4ef3c16f83c455209f03c2f209518a51f6c8388d
weibo <805407646@qq.com>
2024-09-29 14:18:35 +0800

    1

--- src/components/chat/ChatMessageInput.vue
+++ src/components/chat/ChatMessageInput.vue
@@ -9,7 +9,9 @@
         class="message-input"
         :auto-size="{ minRows: 1, maxRows: 3 }"
     />
-    <a-button @click="handleSendOrStop" class="send-button" :disabled="isSending">{{ currentSession.isStreaming ? '终止' : '发送' }}</a-button>
+    <a-button type="primary" @click="handleSendOrStop" :disabled="isSending">
+      {{ currentSession.isStreaming ? '终止' : '发送' }}
+    </a-button>
   </div>
 </template>
 
@@ -49,7 +51,7 @@ export default {
           currentSession.value.messages.push({role: 'user', content: message});
           inputMessage.value = ''; // 清空输入框
           isSending.value = false;
-          await sessionStore.selectFileAndChat(currentSession.value, currentSession.value.messages.length-1, false, false);
+          await sessionStore.selectFileAndChat(currentSession.value, currentSession.value.messages.length - 1, false, false);
         } catch (error) {
           console.error('消息发送失败:', error);
           alert('发送消息时出现问题，请稍后再试。');
@@ -93,51 +95,24 @@ export default {
   display: flex;
   align-items: flex-start;
   width: 100%;
-  background-color: #1a1a1a;
   border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 255, 255, 0.5);
 }
 
 .message-input {
   flex: 1;
   margin-right: 10px;
-  background-color: #2a2a2a;
-  color: #00ffcc;
-  border: 1px solid #00ffcc;
   border-radius: 5px;
   padding: 10px;
   font-family: 'Orbitron', sans-serif;
-  box-shadow: 0 0 5px rgba(0, 255, 255, 0.5);
   transition: all 0.3s ease;
 }
 
 .message-input:focus {
-  border-color: #00ccff;
-  box-shadow: 0 0 10px rgba(0, 255, 255, 0.8);
+  border-color: black;
+  box-shadow: 0 0 10px rgba(2, 2, 2, 0.8);
 }
 
-.send-button {
-  background-color: #00ffcc;
-  color: #1a1a1a;
-  border: none;
-  border-radius: 5px;
-  font-family: 'Orbitron', sans-serif;
-  box-shadow: 0 0 5px rgba(0, 255, 255, 0.5);
-  transition: all 0.3s ease;
-}
-
-.send-button:hover {
-  background-color: #00ccff;
-  box-shadow: 0 0 10px rgba(0, 255, 255, 0.8);
-}
-
-.send-button:active {
-  background-color: #00ccff;
-  box-shadow: 0 0 5px rgba(0, 255, 255, 0.5);
-}
-
-.send-button:focus {
-  outline: none;
-  box-shadow: 0 0 10px rgba(0, 255, 255, 0.8);
+.message-input:hover {
+  border-color: black;
 }
 </style>
