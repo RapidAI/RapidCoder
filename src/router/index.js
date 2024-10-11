f3fd12fbfcdf3ede30b8ee7c06dd775e8ba14bd2
weibo <805407646@qq.com>
2024-10-11 16:53:11 +0800

    干掉

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import NotFound from '../components/common/NotFound.vue';
 import Session from '../components/session/SessionProject.vue';
-import ProjectTable from '../components/project/ProjectTable.vue';
 import ModelTable from '../components/model/ModelTable.vue';
 
 const routes = [
@@ -18,11 +17,6 @@ const routes = [
         path: '/ModelTable',
         name: 'ModelTable',
         component: ModelTable
-    },
-    {
-        path: '/ProjectTable',
-        name: 'ProjectTable',
-        component: ProjectTable
     },
     {
         path: '/:pathMatch(.*)*',
