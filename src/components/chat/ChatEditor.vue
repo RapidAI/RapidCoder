b1dfad8d5c9d2013fc81afd58c458de1b717975d
weibo <805407646@qq.com>
2024-10-17 14:15:03 +0800

    干掉

--- src/components/chat/ChatEditor.vue
+++ src/components/chat/ChatEditor.vue
@@ -10,7 +10,8 @@
           {{ getTitle(file) }}
         </span>
       </template>
-      <code-editor :content="fileContents[index]" :selectedSessionId="selectedSessionId" :language="file.split('.').pop()" :file-path="file"/>
+      <code-editor :content="fileContents[index]" :selectedSessionId="selectedSessionId"
+                   :language="file.split('.').pop()" :file-path="file"/>
     </a-tab-pane>
   </a-tabs>
 </template>
@@ -51,27 +52,62 @@ export default {
 
     watch(
         () => currentSession.value?.currentSelectFile,
-        async (newFiles) => {
+        async (newFiles, oldFiles = []) => {
           if (newFiles?.length) {
+            // 获取新的文件内容
             fileContents.value = await Promise.all(
-                newFiles.map((file) =>
-                    ipcRenderer.invoke("getFileContent", file)
-                )
+                newFiles.map((file) => ipcRenderer.invoke("getFileContent", file))
             );
-            if (!newFiles.includes(activeFile.value)) {
-              activeFile.value = newFiles[0];
-            }
-            const wrappedContents = fileContents.value.map((content, index) =>
-                wrapFileContent(content, newFiles[index])
-            );
-            currentSession.value.messages[0] =  {
+
+            // 设置 activeFile，如果不在新文件列表中，默认第一个
+            activeFile.value = newFiles.includes(activeFile.value) ? activeFile.value : newFiles[0];
+
+            // 更新系统消息内容
+            currentSession.value.messages[0] = {
               role: "system",
-              content: wrappedContents.join("\n"),
+              content: fileContents.value.map((content, index) => wrapFileContent(content, newFiles[index])).join("\n"),
             };
+
+            // 对新增的文件进行监控并处理文件变化和删除事件
+            newFiles.forEach((file) => {
+              if (!oldFiles.includes(file)) {
+                ipcRenderer.invoke('initFileWatch', file, props.selectedSessionId);
+                setupFileWatch(file, newFiles);
+              }
+            });
+
+            // 移除不再选中的文件的监控
+            oldFiles.forEach((file) => {
+              if (!newFiles.includes(file)) {
+                ipcRenderer.invoke('removeFileWatch', file, props.selectedSessionId);
+              }
+            });
           }
         },
         {immediate: true}
     );
+
+// 处理文件变化和删除的函数
+    const setupFileWatch = (file, newFiles) => {
+      ipcRenderer.on(file, async (event, {action, fileInfo}) => {
+        const index = newFiles.indexOf(fileInfo.key);
+        if (action === 'change' && index !== -1) {
+          // 重新获取文件内容并更新
+          const updatedContent = await ipcRenderer.invoke('getFileContent', fileInfo.key);
+          fileContents.value[index] = updatedContent;
+
+          // 更新系统消息
+          currentSession.value.messages[0].content = fileContents.value
+              .map((content, idx) => wrapFileContent(content, newFiles[idx]))
+              .join("\n");
+        } else if (action === 'unlink' && index !== -1) {
+          // 移除删除的文件
+          currentSession.value.currentSelectFile.splice(index, 1);
+          fileContents.value.splice(index, 1);
+        }
+      });
+    };
+
 
     const onEdit = (targetKey, action) => {
       if (action === "remove") {
