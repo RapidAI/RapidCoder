4fc8070ffd71585e27ea9e25548fca51b93897b0
weibo <805407646@qq.com>
2024-10-19 14:40:47 +0800

    干掉

--- src/components/chat/SshConnector.vue
+++ src/components/chat/SshConnector.vue
@@ -3,17 +3,17 @@
 </template>
 
 <script>
-import {Terminal} from '@xterm/xterm';
+import {Terminal} from 'xterm';
 import {WebLinksAddon} from '@xterm/addon-web-links';
-import {AttachAddon} from '@xterm/addon-attach';
-import {ClipboardAddon} from '@xterm/addon-clipboard';
 import {FitAddon} from '@xterm/addon-fit';
+import {ClipboardAddon} from '@xterm/addon-clipboard';
 import {ImageAddon} from '@xterm/addon-image';
 import {SearchAddon} from '@xterm/addon-search';
 import {SerializeAddon} from '@xterm/addon-serialize';
 import {Unicode11Addon} from '@xterm/addon-unicode11';
-import {WebglAddon} from '@xterm/addon-webgl';
 import 'xterm/css/xterm.css';
+
+const {ipcRenderer} = require('electron');
 
 export default {
   props: {
@@ -24,19 +24,32 @@ export default {
   mounted() {
     this.initTerminal();
     window.addEventListener('resize', this.onResize);
+
+    // 启动 Shell 会话
+    ipcRenderer.invoke('start-shell').then((response) => {
+      this.terminal.write(response.message + '\r\n');
+    });
+
+    // 监听 Shell 输出并显示到终端
+    ipcRenderer.on('shell-output', (event, output) => {
+      this.terminal.write(output);
+    });
   },
   beforeDestroy() {
     window.removeEventListener('resize', this.onResize);
     if (this.terminal) {
       this.terminal.dispose();
     }
+    // 停止 Shell 会话
+    ipcRenderer.invoke('stop-shell');
+    ipcRenderer.removeAllListeners('shell-output');
   },
   methods: {
     initTerminal() {
       // 保存 terminal 到 this
       this.terminal = new Terminal({allowProposedApi: true});
       this.terminal.open(this.$refs.terminal);
-      this.terminal.write('SSH Terminal Initialized\r\n');
+      this.terminal.write('Shell Terminal Initialized\r\n');
 
       // 加载所有的插件
       const fitAddon = new FitAddon();
@@ -47,10 +60,14 @@ export default {
       this.terminal.loadAddon(new SearchAddon());
       this.terminal.loadAddon(new SerializeAddon());
       this.terminal.loadAddon(new Unicode11Addon());
-      this.terminal.loadAddon(new WebglAddon());
 
       // 适应初始窗口大小
       fitAddon.fit();
+
+      // 监听用户输入并发送到 Shell
+      this.terminal.onData((data) => {
+        ipcRenderer.invoke('send-shell-command', data);
+      });
     },
     onResize() {
       const fitAddon = this.terminal.getAddon('fit');
